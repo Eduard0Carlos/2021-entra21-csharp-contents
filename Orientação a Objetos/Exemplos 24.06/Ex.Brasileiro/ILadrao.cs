29872263ff9e76c3b607ed24a32c90db74ff8618
Carlos <65471008+relex337@users.noreply.github.com>
2021-06-24 21:48:49 -0300

    Add files via upload

---
+++ Orientação a Objetos/Exemplos 24.06/Ex.Brasileiro/ILadrao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heranca
+{
+    public interface ILadrao
+    {
+        public interface ILadrao
+        {
+            public double ValorRoubado { get; set; }
+
+            public void Roubar(Brasileiro b);
+        }
+    }
+}

3833017765f12a514c13dc4bebfa98db1f923c20
Carlos <65471008+relex337@users.noreply.github.com>
2021-08-26 19:30:39 -0300

    Add files via upload

---
+++ WEB/DataInfrastructure/Mappings/FilmeMapConfig.cs
@@ -0,0 +1,28 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataInfrastructure.Mappings
+{
+    internal class FilmeMapConfig : IEntityTypeConfiguration<Filme>
+    {
+        public void Configure(EntityTypeBuilder<Filme> builder)
+        {
+            builder.Property(f => f.Nome).IsRequired().HasMaxLength(70);
+
+            builder.HasOne(f => f.Genero).WithMany(g => g.Filmes).OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasMany(f => f.Locacao).WithMany(l => l.Filmes).UsingEntity<Dictionary<string, object>>("FilmLocation",
+              j => j.HasOne<Locacao>().WithMany().HasForeignKey("LocationsID").HasConstraintName("FK_FilmLocation_Locations_LocationsID").OnDelete(DeleteBehavior.NoAction),
+              j => j.HasOne<Filme>().WithMany().HasForeignKey("FilmsID").HasConstraintName("FK_FilmLocation_Films_FilmsID").OnDelete(DeleteBehavior.NoAction));
+
+
+
+        }
+    }
+}
---
+++ WEB/Domain/Filme.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Filme
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public int Duracao { get; set; }
+        public int AnoLancamento { get; set; }
+        
+        public int GeneroID { get; set; }
+        public Genero Genero { get; set; } // o mesmo genero pode estar em varios filmes, mas um filme so pode conter um genero
+
+        public ICollection<Locacao> Locacao { get; set; }
+
+        public double preco
+        {
+            get
+            {
+                int ano = DateTime.Now.Year;
+                int diferencaTempo = ano - AnoLancamento;
+                if (ano == 0 )
+                {
+                    return 10;
+                }
+                else if (diferencaTempo < 3)
+                {
+                    return 8;
+                }
+                else if (diferencaTempo < 5)
+                {
+                    return 6;
+                }
+                return 5;
+            }
+        }
+
+    }
+}

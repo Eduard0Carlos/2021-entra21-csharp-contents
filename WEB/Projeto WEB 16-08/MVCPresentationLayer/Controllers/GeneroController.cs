5a8c4d6e24769644fd2297aa20f4604a20a0f9bc
Carlos <65471008+relex337@users.noreply.github.com>
2021-08-26 19:44:06 -0300

    Quinta-Feira 26/08

---
+++ WEB/Projeto WEB 16-08/MVCPresentationLayer/Controllers/GeneroController.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using MVCPresentationLayer.Models.Genero;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCPresentationLayer.Controllers
+{
+    public class GeneroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(GeneroInsertViewModel viewModel)
+        {
+            MapperConfiguration config =
+                new MapperConfiguration(cfg => cfg.CreateMap<GeneroInsertViewModel, Genero>());
+
+            IMapper mapper = config.CreateMapper();
+
+            Genero genero = mapper.Map<Genero>(viewModel);
+
+
+            return View();
+        }
+
+    }
+}
---
+++ WEB/README.md
@@ -0,0 +1,22 @@
+# WEB
+
+* [Entity Framework](#entity-framework)
+* [MVC](#mvc)
+
+## Entity Framework
+
+https://www.entityframeworktutorial.net/
+
+
+### Comandos NuGet
+
+```NuGet
+	Install-Package Microsoft.EntityFrameworkCore
+	Install-Package Microsoft.EntityFrameworkCore.SqlServer
+	Install-Package Microsoft.EntityFrameworkCore.Tools
+	Install-Package Microsoft.EntityFrameworkCore.Design
+```
+
+## MVC
+
+O MVC é uma sigla do termo em inglês Model (modelo) View (visão) e Controller (Controle) que facilita a troca de informações entre a interface do usuário aos dados no banco, fazendo com que as respostas sejam mais rápidas e dinâmicas.

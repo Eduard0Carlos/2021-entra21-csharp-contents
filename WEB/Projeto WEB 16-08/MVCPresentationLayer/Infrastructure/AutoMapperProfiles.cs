bf6d36df6be618215335b4c1638aa6528eaa58e5
Carlos <65471008+relex337@users.noreply.github.com>
2021-08-28 19:42:43 -0300

    Atualizado Sexta-Feira 27/08

---
+++ WEB/Projeto WEB 16-08/MVCPresentationLayer/Infrastructure/AutoMapperProfiles.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Domain;
+using MVCPresentationLayer.Models.Genero;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCPresentationLayer.Infrastructure
+{
+	public class GenericProfile : Profile
+	{
+		public GenericProfile()
+		{
+			CreateMap<GeneroInsertViewModel, Genero>();
+		}
+	}
+}
---
+++ WEB/Projeto WEB 16-08/Service/Validations/GeneroValidation.cs
@@ -0,0 +1,19 @@
+﻿using Domain;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Validations
+{
+    class GeneroValidation : AbstractValidator<Genero>
+    {
+        public GeneroValidation()
+        {
+            RuleFor(g => g.Nome).NotEmpty().WithMessage("Nome deve ser informado.");
+            RuleFor(g => g.Nome).Length(3, 50).WithMessage("Nome deve conter entre 3 e 50 caracteres.");
+        }
+    }
+}
